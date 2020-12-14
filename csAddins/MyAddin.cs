6781b59d447e0b285763e34a2db1ec19be235672
Marek Praski <marekpraski@wp.pl>
2020-12-14 20:00:00 +0100

    pierwszy commit; po skompilowaniu i uruchomieniu w Microstation komendą mdl load dll wyświetla okno komunikatu z informacją o wersji .Net

---
+++ csAddins/MyAddin.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace csAddins
+{
+    internal sealed class MyAddin : Bentley.MicroStation.AddIn
+    {
+        private MyAddin(System.IntPtr mdlDesc)
+            : base(mdlDesc)
+        {
+        }
+        protected override int Run(string[] commandLine)
+        {
+            //MessageBox.Show("Hello World");
+            string sWinFrameworkPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+            MessageBox.Show("Framework Path =" + sWinFrameworkPath);
+            return 0;
+        }
+    }
+}

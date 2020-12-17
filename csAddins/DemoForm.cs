dd54ff539d76e727c966ba59c7cb617738ed50d3
Marek Praski <marekpraski@wp.pl>
2020-12-17 20:23:56 +0100

    część 7 tutoriala, okno reagujące na zmianę warstw Microstation

--- csAddins/DemoForm.cs
+++ csAddins/DemoForm.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using Bentley.MicroStation.InteropServices;
 using Bentley.Interop.MicroStationDGN;
 using BCOM = Bentley.Interop.MicroStationDGN;
+using Bentley.MicroStation;
 
 namespace csAddins
 {
@@ -177,6 +178,59 @@ namespace csAddins
             }
             app.ActiveSettings.AnnotationScaleEnabled = false;
             app.CommandState.StartPrimitive(new NoteCoordClass(), false);
+        }
+
+// ----------------------------------------------------------------------------------------------------------
+
+        public class LevelChangedClass : ILevelChangeEvents
+        {
+            public void LevelChanged(MsdLevelChangeType ChangeType, Level TheLevel, ModelReference TheModel)
+            {
+                if (MsdLevelChangeType.AfterCreate == ChangeType ||
+                    MsdLevelChangeType.AfterDelete == ChangeType ||
+                    MsdLevelChangeType.ChangeName == ChangeType)
+                    PopulateLevelList();
+            }
+            public static void MyAddin_NewDesignFileEvent
+                               (AddIn sender, AddIn.NewDesignFileEventArgs eventArgs)
+            {
+                if (AddIn.NewDesignFileEventArgs.When.AfterDesignFileOpen == eventArgs.WhenCode)
+                    PopulateLevelList();
+            }
+            private static void PopulateLevelList()
+            {
+                LevelChangedForm myLevelChangedForm = null;
+                foreach (Form myForm in System.Windows.Forms.Application.OpenForms)
+                    if ("LevelChangedForm" == myForm.Name)
+                    {
+                        myLevelChangedForm = (LevelChangedForm)myForm;
+                        break;
+                    }
+                if (null != myLevelChangedForm)
+                {
+                    myLevelChangedForm.listBox1.Items.Clear();
+                    foreach (Level myLvl in Utilities.ComApp.ActiveDesignFile.Levels)
+                        myLevelChangedForm.listBox1.Items.Add(myLvl.Name);
+                }
+            }
+        }
+
+        public static LevelChangedClass myLevelChanged = null;
+        public static AddIn.NewDesignFileEventHandler myNewDGNHandler = null;
+        private static LevelChangedForm myLevelForm = null;
+        public static void LevelChanged(string unparsed)
+        {
+            if (null == myLevelForm || myLevelForm.IsDisposed)
+            {
+                myLevelForm = new LevelChangedForm();
+                myLevelForm.AttachAsTopLevelForm(MyAddin.s_addin, false);
+                myLevelForm.Show(); myLevelChanged = new LevelChangedClass();
+                Utilities.ComApp.AddLevelChangeEventsHandler(myLevelChanged); myNewDGNHandler = new AddIn.NewDesignFileEventHandler
+                         (LevelChangedClass.MyAddin_NewDesignFileEvent);
+                MyAddin.s_addin.NewDesignFileEvent += myNewDGNHandler;
+            }
+            else
+                myLevelForm.Activate();
         }
     }
 }
